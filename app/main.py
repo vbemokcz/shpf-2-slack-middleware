50b4d9bc3640abb8279a631df4c54ffc9255044f
vbemokcz <sp09.es@okaycz.eu>
2022-05-23 13:03:53 +0200

    fix postgres uri

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -6,7 +8,7 @@ from app.slack_sender import SlackSender
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///webhooks'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
