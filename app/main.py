bdc171928d83368127e5119a01e9cd0cfe19ae15
vbemokcz <sp09.es@okaycz.eu>
2022-05-23 13:03:26 +0200

    add postgres db to store webhooks

--- app/main.py
+++ app/main.py
@@ -1,51 +1,14 @@
-import os
-import smtplib
-
-from flask import Flask, request, json
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 from app.slack_sender import SlackSender
 
 
-SLACK_TOKEN = os.environ.get('SLACK_TOKEN')
-SLACK_CHANNEL = os.environ.get('SLACK_CHANNEL')
-
-
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///webhooks'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@app.route('/', methods=['GET'])
-def index():
-    return '<h1><a href="https://github.com/malchikkCZ/shpf-2-slack-middleware">SHPF-2-SLCK MiddleWare</a></h1>'
 
-@app.route('/theme-publish/<auth_hash>', methods=['POST'])
-def theme_publish_endpoint(auth_hash):
-    if auth_hash == os.environ.get('AUTH_HASH'):
-        data = request.json
-        theme_id = data['id']
-        theme_title = data['name']
-        shop_name = request.headers.get('X-Shopify-Shop-Domain')
-
-        SLACK = SlackSender(SLACK_TOKEN, SLACK_CHANNEL)
-        SLACK.send(f'New master theme *{theme_title}* id *{theme_id}* has been published at {shop_name}.')
-
-        return 'Success', 200
-
-    return 'Failure', 500
-
-@app.route('/draft_order/<auth_hash>', methods=['POST'])
-def draft_order_endpoint(auth_hash):
-    if auth_hash == os.environ.get('AUTH_HASH'):
-        data = request.json
-
-        with smtplib.SMTP('smtp.gmail.com') as mailserver:
-            mailserver.starttls()
-            mailserver.login(user=os.environ.get('MAIL_FROM'), password=os.environ.get('MAIL_PASS'))
-            mailserver.sendmail(
-                from_addr=os.environ.get('MAIL_FROM'),
-                to_addrs=os.environ.get('MAIL_TO'),
-                msg=f'Subject:Webhook test message\n\n{json.dumps(data)}'
-            )
-
-        return 'Success', 200
-
-    return 'Failure', 500
+from app import routes
---
+++ app/routes.py
@@ -0,0 +1,61 @@
+import os
+
+from flask import request
+
+from app.main import app, db
+from app.models import DraftOrder
+from app.slack_sender import SlackSender
+
+
+SLACK_TOKEN = os.environ.get('SLACK_TOKEN')
+SLACK_CHANNEL = os.environ.get('SLACK_CHANNEL')
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return '<h1><a href="https://github.com/malchikkCZ/shpf-2-slack-middleware">SHPF-2-SLCK MiddleWare</a></h1>'
+
+
+@app.route('/theme-publish/<auth_hash>', methods=['POST'])
+def theme_publish_endpoint(auth_hash):
+    if auth_hash == os.environ.get('AUTH_HASH'):
+        data = request.json
+        theme_id = data['id']
+        theme_title = data['name']
+        shop_name = request.headers.get('X-Shopify-Shop-Domain')
+
+        SLACK = SlackSender(SLACK_TOKEN, SLACK_CHANNEL)
+        SLACK.send(f'New master theme *{theme_title}* id *{theme_id}* has been published at {shop_name}.')
+
+        return 'Success', 200
+
+    return 'Failure', 500
+
+
+@app.route('/draft-order/<shop_name>', methods=['GET'])
+def draft_order_get_endpoint(shop_name):
+
+    draft_order = DraftOrder.query.filter_by(shop=shop_name)[-1]
+
+    if draft_order:
+        return f'{draft_order.created_at}'
+
+    return 'Not found', 404
+
+
+@app.route('/draft-order/<auth_hash>', methods=['POST'])
+def draft_order_post_endpoint(auth_hash):
+    if auth_hash == os.environ.get('AUTH_HASH'):
+        data = request.json
+
+        draft_order = DraftOrder(
+            shop = request.headers.get('X-Shopify-Shop-Domain'),
+            created_at = data['created_at'],
+            order_name = data['name']
+        )
+
+        db.session.add(draft_order)
+        db.session.commit()
+        return 'Success', 200
+
+    return 'Failure', 500
